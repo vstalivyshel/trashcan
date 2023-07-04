6ecbe6913f3f4af1f7b1e2b8de649f99aa1df5a2
vstalivyshel <112723363+vstalivyshel@users.noreply.github.com>
2023-07-04 15:08:23 +0300

    yea

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     fish_add_path /usr/local/texlive/2023/bin/x86_64-linux
-    set -xga PKG_CONFIG_PATH /usr/lib64/pkgconfig /usr/lib/x86_64-linux-gnu/pkgconfig 
+    set -xga PKG_CONFIG_PATH /usr/lib64/pkgconfig /usr/lib/x86_64-linux-gnu/pkgconfig
+    set -x KAKOUNE_POSIX_SHELL /bin/bash
     set fish_cursor_default block
     set -Ux EDITOR kak
     set -Ux VISUAL $EDITOR
@@ -9,9 +10,7 @@ if status is-interactive
     bind \cl 'ls -Ahls; commandline -f repaint'
     setxkbmap -option "ctrl:nocaps"
 
-    set -g PYTHONDONTWRITEBYTECODE 1
-
-    alias up='sudo dnf update -y; rustup update; brew upgrade'
+    alias up='sudo apt update -y && sudo pat upgrade; rustup update; brew upgrade'
     alias dfs="/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME"
     alias dfsdo='dfs commit -a -m "yea"; dfs push origin main'
 
